bcea8849b35d1f374583f77385946e9790f7fef5
Tim Dolan <timmydolan14@gmail.com>
2023-06-01 16:32:20 -0400

    Updated Webpack

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -2,6 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
@@ -26,6 +27,45 @@ module.exports = () => {
       new InjectManifest({
         swSrc: './src/sw.js',
         swDest: 'service-worker.js',
+      }),
+      new WebpackPwaManifest({
+        name: 'My Progressive Web App',
+        short_name: 'MyPWA',
+        description: 'My awesome Progressive Web App!',
+        background_color: '#ffffff',
+        crossorigin: 'use-credentials',
+        icons: [
+          {
+            src: path.resolve('src/assets/icon.png'),
+            sizes: [96, 128, 192, 256, 384, 512] // multiple sizes
+          },
+          {
+            src: path.resolve('src/assets/large-icon.png'),
+            size: '1024x1024' // you can also use the specifications pattern
+          },
+          {
+            src: path.resolve('src/assets/maskable-icon.png'),
+            size: '1024x1024',
+            purpose: 'maskable'
+          }
+        ]
+      }),
+      new WorkboxPlugin.GenerateSW({
+        // Do not precache images
+        exclude: [/\.(?:png|jpg|jpeg|svg)$/],
+        // Define runtime caching rules.
+        runtimeCaching: [{
+          // Match any request that ends with .png, .jpg, .jpeg or .svg.
+          urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
+          handler: 'CacheFirst',
+          options: {
+            // Use a custom cache name.
+            cacheName: 'images',
+            expiration: {
+              maxEntries: 100,
+            },
+          },
+        }],
       }),
     ],
     module: {
